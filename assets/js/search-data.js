13cd7451a2a069a348f098ba3bc5ad382667b752
CQU-yxy <62419899+CQU-yxy@users.noreply.github.com>
2025-02-26 09:57:00 +0000

    Deploying to gh-pages from @ CQU-yxy/CQU-yxy.github.io@a8ad20cda9039aaf833e1ce57dc7d2ef7629899a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "You can download the my CV which is updated to Feb. 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
